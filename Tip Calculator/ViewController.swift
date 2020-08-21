ec10582df24f8a085c77bf9b64a953c5af7517e5
Abhay Naik <abhay.naik@hotmail.com>
2020-08-21 10:24:36 -0700

    Initial Commit

---
+++ Tip Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip Calculator
+//
+//  Created by Abhay Naik on 8/21/20.
+//  Copyright © 2020 Abhay Naik (CodePath). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
